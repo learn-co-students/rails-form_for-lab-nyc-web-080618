b71f522a6f65c848d1f789c656bb30fa59849ce4
mguevara717 <marissa.guevara717@gmail.com>
2018-09-03 16:04:06 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+students = Student.create([
+    {first_name: "FNAME", last_name: "LNAME"},
+    {first_name: "Marissa", last_name: "Orea"},
+    {first_name: "Jose", last_name: "Orea"}
+  ])
+
+school_classes = SchoolClass.create ([
+  {title: "Title", room_number: 212},
+  {title: "Title", room_number: 305},
+  {title: "Title", room_number: 101}
+
+  ])
